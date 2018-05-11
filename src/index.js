4f59b917bcd3bc670229b0daf0b259dd0208b43e
German Nicolas Olle <germanolle.unlp@gmail.com>
2018-05-11 14:12:30 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>React Training</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<Index />, document.getElementById("index"));
